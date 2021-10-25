a6e36ad7612bce02fc749af7a111ca94e70027f1
jennycho1 <63076177+jennycho1@users.noreply.github.com>
2021-10-24 20:34:59 -0700

    unity project upload

---
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Vector3.up is same as (0,1,0). Time.deltaTime for frame independent speed
+            // Relative force add force relative to object's direction
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            // SFX
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else {
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        // physics system conflicts with update. so freeze physics system when controlling
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+
+}
